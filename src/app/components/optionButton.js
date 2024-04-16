2255ac24074d32b43a519d9534503e0db6ab570c
Reid Taylor <reidhtaylor@icloud.com>
2024-04-16 14:32:18 -0400

    Fixed the help message

--- src/app/components/optionButton.js
+++ src/app/components/optionButton.js
@@ -1,7 +1,7 @@
 "use client";
 
 const OptionButton = ({content, col}) => {
-  return (<button className={`rounded-lg ${col ? col : "bg-zinc-800/20 " } px-5 py-2 hover:opacity-80 active:opacity-50`} onClick={() => alert("Hello")}>
+  return (<button className={`rounded-lg ${col ? col : "bg-zinc-800/20 " } px-5 py-2 hover:opacity-80 active:opacity-50`} onClick={() => alert("Write it down lol...")}>
     <h2 className={`text-sm text-white font-semibold`}>{content}</h2>
   </button>)
 }
